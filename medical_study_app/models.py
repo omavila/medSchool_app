8e409956975bab29ba63bdb748ad183abdf540c0
Oscar Mavila <omavila@uccs.edu>
2023-11-04 17:10:24 -0600

    database done

--- medical_study_app/models.py
+++ medical_study_app/models.py
@@ -10,32 +10,31 @@ class Client(models.Model):
     def __str__(self):
         return self.name
     
-   # def get_absolute_url(self):
-   #     return reverse('client-detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('client-detail', args=[str(self.id)])
     
 class Notegroup(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     is_private = models.BooleanField(default=False)
-    about = models.TextField(blank=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE, default = None)
 
     def __str__(self):
         return self.title
     
-  #  def get_absolute_url(self):
-   #     return reverse('notegroup-detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('notegroup-detail', args=[str(self.id)])
     
 class Notecard(models.Model):
     
     title = models.CharField(max_length=200)
     definition = models.TextField(blank=False)
     client = models.ForeignKey(Client, on_delete=models.CASCADE, default = None)
-    notegroup = models.ManyToManyField(Notegroup, blank=True, null=True)
+    notegroup = models.ManyToManyField(Notegroup, blank=True)
     
     def __str__(self):
         return self.title
     
-   # def get_absolute_url(self):
-    #    return reverse('notecard-detail', args=[str(self.id)])
+    def get_absolute_url(self):
+       return reverse('notecard-detail', args=[str(self.id)])
     
