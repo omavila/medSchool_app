e8c993a8a3c5180f97aa72fe7b42c40801df9cdf
Oscar Mavila <omavila@uccs.edu>
2023-11-04 16:43:32 -0600

    set up files

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -124,7 +125,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/images/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- medical_study_app/models.py
+++ medical_study_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Client(models.Model):
+
+    name = models.CharField(max_length=200)
+    email = models.CharField("Email: ", max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+class Notegroup(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    is_private = models.BooleanField(default=False)
+    about = models.TextField(blank=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, default = None)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('notegroup-detail', args=[str(self.id)])
+    
+class Notecard(models.Model):
+    
+    title = models.CharField(max_length=200)
+    definition = models.TextField(blank=False)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, default = None)
+    notegroup = models.ManyToManyField(Notegroup, blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('notecard-detail', args=[str(self.id)])
+    
